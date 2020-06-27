a25c5c642b9dea6253e28af3b06c5cba4bd5f0a6
木田壱政 <kidaissei@MacBook-Pro.local>
2020-06-28 05:43:17 +0900

    Firebaseのタスクを更新、add_task_pageで登録と更新の表示を動的に変更できるようにした

--- lib/domain/task.dart
+++ lib/domain/task.dart
@@ -1,5 +1,11 @@
 // エンティティモデル(インスタンスフィールド)
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Task {
-  Task(this.title);
+  Task(DocumentSnapshot doc) {
+    documentID = doc.documentID;
+    title = doc['title'];
+  }
+  String documentID;
   String title;
 }
